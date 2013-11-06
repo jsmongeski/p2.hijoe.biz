6ed2ca50b4ad9213ea1c62d884f152b26d95e830
Joe Smongeski <jsmongeski@g.harvard.edu>
2013-11-06 08:10:42 -0500

    updates for p2

--- config/config.php
+++ config/config.php
@@ -14,11 +14,11 @@ If you want a different default time format for this app, set it below
 */
 
 # What is the name of this app?
-	define('APP_NAME', 'Sample App');  
+	define('APP_NAME', 'Homebrewbuzz');  
 
 # When email is sent out from the server, where should it come from?
 # Ideally, this should match the domain name
-	define('APP_EMAIL', 'webmaster@sample-app.com'); 
+	define('APP_EMAIL', 'webmaster@homebrewbuzz.com'); 
 
 /* 
 A email designated to receive messages from the server. Examples:
@@ -28,20 +28,20 @@ A email designated to receive messages from the server. Examples:
  	
  	Some might want this to be the same as the APP_EMAIL, others might want to create a designated gmail address for it
 */ 	
-	define('SYSTEM_EMAIL', 'webmaster@myapp.com'); 
+	define('SYSTEM_EMAIL', 'webmaster@homebrewbuzz.com'); 
 
 # Default DB name for this app
-	define('DB_NAME', "myapp_com"); 
+	define('DB_NAME', "p2_hijoe_biz"); 
 
 # Timezone
 	define('TIMEZONE', 'America/New_York');
 
 # If your app is going to have outgoing emails, you should fill in your SMTP settings
 # For this you could use gmail SMTP or something like http://sendgrid.com/
-	//define('SMTP_HOST', '');
-	//define('SMTP_USERNAME', '');
-	//define('SMTP_PASSWORD', '');
+	define('SMTP_HOST', 'smtp.gmail.com');
+	define('SMTP_USERNAME', 'jsmonges@gmail.com');
+	define('SMTP_PASSWORD', 'taichi1gmA');
 
 # For extra security, you might want to set different salts than what the core uses
-	//define('PASSWORD_SALT', '');
-	//define('TOKEN_SALT', '');
+	//define('PASSWORD_SALT', 'lqsv6Z0SAk3E3SIL5@oIzIz2%i$aEBTIavC8K6AF');
+	//define('TOKEN_SALT', '   zqsv6Z0Spbk3E3SIL57oIzIz2qB@EBTIavC8K6AF');
